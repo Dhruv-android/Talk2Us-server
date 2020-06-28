0987bbd3c5e286f50ddbd4ba44dbdc836bda6eba
pallav dubey <rajeevpallav606@gmail.com>
2020-06-28 19:37:38 +0530

    Migrated new chats stablished to functions

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
+const { database } = require('firebase-admin');
 admin.initializeApp();
-console.log("Hello","sucess")
-exports.checkflag = functions.database.ref()
+exports.checkflag = functions.database.ref('/chatMessages/')
 .onUpdate((snapshot, context) => {
         const temptoken = 'c4VgIqxn8o0:APA91bGIXejm1XG01-udOnphkXZxebMP0M0IQ0GC-7c1tsvKHsK9z48dcgLZTsPfxEBIZB3qJtg2p2RcDZGe8U9XvR3b_seIpLK84-jeP7KybpH1Ac0wScclSxRsx2RACNLsHhpJQGhU';  
         const flag = snapshot.after.val();
-        let statusMessage = `Message from the clouds as ${flag}`
+        let statusMessage = ``
+        
         var message = {
             notification: {
                 title: 'cfunction',
@@ -23,3 +24,10 @@ exports.checkflag = functions.database.ref()
             
         });
     });
+exports.newChatStablised=functions.database.ref('/counsellorChats/{counsellorId}')
+.onUpdate((snapshot,context)=>{
+    const cid=context.params.counsellorId
+    const count=snapshot.after.numChildren()
+    const parent =snapshot.after.ref.parent.parent.child('Counsellor').child(cid).child('clients')
+    return parent.set(count);
+});
